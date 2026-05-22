419fd044704e7ee0f780d48a9d9d3b4c5e2ba2bc
Vishal modhvadiya <vishal6619modhvadiya@gamil.com>
2026-05-22 15:47:29 +0530

    updated

--- app/services/gemini_service.py
+++ app/services/gemini_service.py
@@ -3,7 +3,7 @@ from app.core.config import settings
 
 client = genai.Client(api_key=settings.GEMINI_API_KEY)
 
-async def get_gemini_response(user_message: str) -> str:
+async def get_gemini_response(user_message: str, session_id: str) -> str:
     response = client.models.generate_content(
         model="models/gemini-2.5-flash",
         contents=user_message
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from sys import exception
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from google.genai.errors import ClientError
@@ -7,14 +8,13 @@ from pydantic import BaseModel
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins =["http://127.0.0.1:8000/"],
+    allow_origins =["http://localhost:3000", "http://127.0.0.1:3000"],
     allow_methods =["*"],
     allow_headers=["*"],
 )
 class ChatRequest(BaseModel):
     message: str
     session_id: str
-
 
 @app.post("/chat")
 async def chat(request: ChatRequest):
@@ -31,3 +31,17 @@ async def chat(request: ChatRequest):
             status_code=500,
             detail="Internal server error"
         ) from exc
+
+@app.delete("/chat/{session_id}")
+async def delete_session(session_id: str):
+    try :  
+        return {"message": f"session{session_id} deleted successfully"}
+    except Exception as exc:
+        raise HTTPException(
+            status_code=500,
+            detail="internal server error"
+        )from exc
+    
+@app.get("/chat")
+async def root():
+    return 
