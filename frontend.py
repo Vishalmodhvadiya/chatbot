c11d1c62c943fb60d4fcfd8634495d3882567aad
Vishal modhvadiya <vishal6619modhvadiya@gamil.com>
2026-05-27 10:16:41 +0530

    updated

--- frontend.py
+++ frontend.py
@@ -3,72 +3,102 @@ import requests
 import uuid
 
 st.set_page_config(
-    page_title = "Chatbot",
-    layout = "wide"
+    page_title="Chatbot",
+    layout="wide"
 )
 
 st.title("chatbot")
 
-with st.sidebar:
-  st.title("chats")
-  if st.button("chat"):
-   st.session_state.messages = []
-   st.session_state.session_id = str(uuid.uuid4())
+if "chats" not in st.session_state:
+    st.session_state.chats = {}
 
+if "session_id" not in st.session_state:
+
+    session_id = str(uuid.uuid4())
+
+    st.session_state.session_id = session_id
+
+    st.session_state.chats[session_id] = []
+
+
+with st.sidebar:
+
+    st.title("Chats")
+
+    if st.button("New Chat"):
+
+        session_id = str(uuid.uuid4())
+
+        st.session_state.session_id = session_id
+
+        st.session_state.chats[session_id] = []
+
+    st.divider()
+
+    for chat_id, messages in st.session_state.chats.items():
+
+        if len(messages) > 0:
+            title = messages[0]["content"][:20]
+        else:
+            title = "New Chat"
+
+        if st.button(title, key=chat_id):
+            st.session_state.session_id = chat_id
+
+messages = st.session_state.chats[st.session_state.session_id]
 
 st.write("current chat")
 
+for msg in messages:
 
-
-if "session_id" not in  st.session_state:
-    st.session_state.session_id = str(uuid.uuid4())
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-
-for msg in st.session_state.messages:
     if msg["role"] == "user":
-        st.markdown(f"**you**: {msg['content']}")
+        st.write(f"<span style='color: purple'> you: {msg['content']}</span>",unsafe_allow_html=True)
     else:
-        st.markdown(f"**bot**: {msg['content']}")
+        st.write(f"<span style='color: green'>bot: {msg['content']}</span>",unsafe_allow_html=True,)
 
 text = st.chat_input("type your message here")
 
 if text:
-    st.session_state.messages.append(
-        {"role": "user",
-         "content" : text}
-    )
+
+    messages.append({
+        "role": "user",
+        "content": text
+    })
 
     with st.chat_message("user"):
-     st.markdown(text)
+        st.write(text)
 
-    payload = {
-    "message" : text,
-    "session_id" : st.session_state.session_id
+    data = {
+        "message": text,
+        "session_id": st.session_state.session_id
     }
 
-    try :
-       response = requests.post("http://localhost:8000/chat", json=payload)
-       response_data = response.json()
-       st.write(response_data)
+    try:
 
-       if "reply" in response_data:
+        response = requests.post(
+            "http://localhost:8000/chat",
+            json=data
+        )
 
-         st.session_state.messages.append(
-          {
-            "role": "assistant",
-            "content": response_data["reply"]
-          }
-         )
+        response_data = response.json()
 
-         with st.chat_message("assistant"):
-          st.markdown(response_data["reply"])
+        if "reply" in response_data:
 
-       else:
-         st.error(response_data.get("detail", "Unknown error"))
+            messages.append({
+                "role": "assistant",
+                "content": response_data["reply"]
+            })
 
+            with st.chat_message("assistant"):
+                st.write(response_data["reply"])
+
+        else:
+            st.error(
+                response_data.get(
+                    "detail",
+                    "Unknown error"
+                )
+            )
 
     except Exception as e:
-      st.error(f"Error: {e}")
+        st.error(f"Error: {e}")
