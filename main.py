2c610847b773e3d0c1f48df5836cc86ef8916640
Vishal modhvadiya <vishal6619modhvadiya@gamil.com>
2026-06-02 14:18:11 +0530

    Fix Render deployment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from sys import exception
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from google.genai.errors import ClientError
-from requests import Session
 from app.services.gemini_service import get_all_sessions_with_messages, get_gemini_response, delete_session
 from pydantic import BaseModel 
 
@@ -22,18 +19,14 @@ async def chat(request: ChatRequest):
     try:
         reply = await get_gemini_response(request.message, request.session_id)
         return {"reply": reply, "session_id": request.session_id}
-    except ClientError as exc:
-        raise HTTPException(
-            status_code=502,
-            detail=f"Gemini API error: {exc}"
-        ) from exc
     except Exception as exc:
-     print("ERROR:", exc)
-    
-     raise HTTPException(
+        print("ERROR:", exc)
+
+        raise HTTPException(
         status_code=500,
         detail=str(exc)
     ) from exc
+
     
 @app.delete("/chat/{session_id}")
 async def delete_chat_session(session_id: str):
